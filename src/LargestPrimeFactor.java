84d14f17a2b22f2b429a38486920cb87f69cd8fc
Kateryna Osadchuk <tug01821@temple.edu>
2018-08-24 11:41:14 -0400

    fixed code

--- src/LargestPrimeFactor.java
+++ src/LargestPrimeFactor.java
@@ -4,25 +4,29 @@ public class LargestPrimeFactor {
 	public static int largestPrime(long num){
 		int largestPrime = 1;
 		
-		for(int i=2; i <= num; i++){
-			if(num % i == 0){
-				for(int l = 1; l <= Math.sqrt(i); l++){
-                    if (i % l == 0){
-                        break;
-                    } else
-                    	 while(num%i==0){
-                             num=num/i;
-                             largestPrime = i;
-                    	 }
-                     
-				}
+		while(num % 2 == 0){
+			largestPrime = 2;
+			num = num/2;
+		} 
+
+		for(int i = 3; i <=num; i+=2){
+			while(num % i == 0){
+				largestPrime = i;
+				num = num/i;
+
 			}
-		
 		}
+
 		return largestPrime;
 	}
 	
 	public static void main(String args[]){
-		System.out.println(largestPrime(15));
+		System.out.println(largestPrime(23));
+		System.out.println(largestPrime(2334));
+		System.out.println(largestPrime(333));
+		System.out.println(largestPrime(104));
+		System.out.println(largestPrime(5));
+		System.out.println(largestPrime(99));
+		System.out.println(largestPrime(8));
 	}
 }
